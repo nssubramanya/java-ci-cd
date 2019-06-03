a30fced24f4abbe85966505af8d50f56849b01eb
Subramanya Narasandra <subramanyans@Subramanyas-MacBook-Pro-3.local>
2019-06-03 15:09:51 +0530

    First commit - Added Hello World Maven Project

---
+++ src/hello-world/src/main/java/com/subbu/java/hello_world/HelloWorldApp.java
@@ -0,0 +1,13 @@
+package com.subbu.java.hello_world;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorldApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
